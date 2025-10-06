c7467c20fab22c83a80392c2f0566ea170b0c884
ArashPoorazam <arashpoorazam88@gmail.com>
2025-10-06 21:28:18 +0330

    Start

---
+++ main.py
@@ -0,0 +1,19 @@
+import pygame
+from constants import *
+
+
+def main():
+    pygame.init()
+
+    while True:    
+        screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+        screen.fill(40)
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+            
+        pygame.display.flip()
+
+if __name__ == "__main__":
+    main()
